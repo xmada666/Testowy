5139c8a10f776a6e08244433a7e7c2388644c655
Artur Adamski <aarrttoorr@gmail.com>
2020-11-28 09:42:16 +0100

    ddd

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        IntStream.range(0,10)
+                .forEach(System.out::println);
+    }
+}
